908c26c764d969c7f4d6db511ffce1ba03cce1a1
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-09-11 23:14:35 -0600

    chore: add project base

---
+++ src/main/java/com/mromer/customer_microservice/CustomerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mromer.customer_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerMicroserviceApplication.class, args);
+	}
+
+}
