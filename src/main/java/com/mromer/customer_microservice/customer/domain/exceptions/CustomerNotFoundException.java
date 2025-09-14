382c35b599b45234ae02ed192a4e0737739f20aa
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-09-14 12:33:09 -0600

    feat: update customers domain

--- src/main/java/com/mromer/customer_microservice/customer/domain/exceptions/CustomerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.mromer.customer_microservice.customer.domain.exceptions;
-
-public class CustomerNotFoundException extends RuntimeException {
-    
-    public CustomerNotFoundException(String id) {
-        super("Cliente con el ID " + id + " no encontrado.");
-    }
-    
-}
--- src/main/java/com/mromer/customer_microservice/customer/domain/exceptions/DuplicateNITException.java
+++
@@ -1,8 +0,0 @@
-package com.mromer.customer_microservice.customer.domain.exceptions;
-
-public class DuplicateNITException extends RuntimeException {
-    public DuplicateNITException(String nit) {
-        super("Un cliente con el NIT '" + nit + "' ya existe.");
-    }
-    
-}
